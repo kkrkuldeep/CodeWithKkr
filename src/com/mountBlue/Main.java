d62881acc0e0afcf5ffa22e35388db04e5ca3431
kuldeep <kkrkuldeep@gmail.com>
2020-02-21 00:05:38 +0530

    Change Code to OddEven

--- src/com/mountBlue/Main.java
+++ src/com/mountBlue/Main.java
@@ -1,9 +1,20 @@
 package com.mountBlue;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello World!");
+        //System.out.println("hello World!");
+        Scanner sn = new Scanner(System.in);
+        int num = sn.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+
     }
 }
